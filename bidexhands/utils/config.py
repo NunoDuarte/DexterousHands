f75a6a50829ac7b64defd6588637b4907f97303e
NunoDuarte <ferreiraduarte.nuno@gmail.com>
2024-04-19 13:44:46 +0100

    update things to make the project run on isaacg

--- bidexhands/utils/config.py
+++ bidexhands/utils/config.py
@@ -82,6 +82,12 @@ def retrieve_cfg(args, use_rlg_config=False):
     elif args.task in ["ShadowHandMetaMT1"]:
         return os.path.join(args.logdir, "{}/{}/{}".format(args.task, args.algo, args.algo)), "cfg/{}/config.yaml".format(args.algo), "cfg/meta_env_cfg/{}.yaml".format(args.task)
 
+    elif args.task in ["ShadowHandPCD"]:
+        return os.path.join(args.logdir, "{}/{}/{}".format(args.task, args.algo, args.algo)), "cfg/{}/config.yaml".format(args.algo), "cfg/{}.yaml".format(args.task)
+        
+    elif args.task in ["ShadowHandPCDtest"]:
+        return os.path.join(args.logdir, "{}/{}/{}".format(args.task, args.algo, args.algo)), "cfg/{}/config.yaml".format(args.algo), "cfg/{}.yaml".format(args.task)
+        
     else:
         warn_task_name()
 
--- bidexhands/utils/parse_task.py
+++ bidexhands/utils/parse_task.py
@@ -25,6 +25,8 @@ from bidexhands.tasks.shadow_hand_pen import ShadowHandPen
 from bidexhands.tasks.shadow_hand_re_orientation import ShadowHandReOrientation
 from bidexhands.tasks.shadow_hand_kettle import ShadowHandKettle
 from bidexhands.tasks.shadow_hand_block_stack import ShadowHandBlockStack
+from bidexhands.tasks.shadow_hand_point_cloud import ShadowHandPCD
+from bidexhands.tasks.shadow_hand_point_cloud2 import ShadowHandPCDtest
 
 # Allegro hand
 from bidexhands.tasks.allegro_hand_over import AllegroHandOver
